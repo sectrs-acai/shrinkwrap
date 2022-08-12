dff1196bdf436fe0a725966ab4851b6179922b69
Ryan Roberts <ryan.roberts@arm.com>
2022-08-12 11:56:21 +0100

    Add top-level shrinkwrap program boilerplate.
    This will eventually act as the main entry point and will dispatch
    to the appropriate command.
    
    Signed-off-by: Ryan Roberts <ryan.roberts@arm.com>

---
+++ shrinkwrap.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+
+# Fixup PythonPath to avoid confusion between (this) module and package.
+import sys
+import os
+root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path = [p for p in sys.path if os.path.basename(p) != 'shrinkwrap']
+sys.path = [root] + sys.path
+
+
+import argparse
+import shutil
+
+
+def formatter(prog):
+	width = shutil.get_terminal_size().columns
+	width -= 2
+	width = min(80, width)
+	return argparse.HelpFormatter(prog, width=width)
+
+
+def main():
+	"""
+	Main entry point. Parses command line and dispatches to appropriate
+	command handler.
+	"""
+
+	tool_name = os.path.splitext(os.path.basename(__file__))[0]
+
+	# Parse the arguments. The parser will raise an exception if the
+	# required arguments are not present, which will tell the user what they
+	# did wrong.
+
+	parser = argparse.ArgumentParser(epilog='To file a bug report, contact '
+						'<ryan.roberts@arm.com>.',
+					 formatter_class=formatter)
+
+	subparsers = parser.add_subparsers(dest='command',
+					   metavar='<command>',
+					   title=f'Supported commands (run '
+						 f'"{tool_name} <command> '
+						 f'--help" for more info')
+
+	# Register all the commands.
+	cmds = {}
+
+	# Parse the arguments.
+	args = parser.parse_args()
+
+	# Dispatch to the correct command.
+	if args.command in cmds:
+		cmds[args.command].dispatch(args)
+	else:
+		print(f'Unknown command {args.command}')
+		parser.print_help()
+
+
+if __name__ == "__main__":
+	main()
+
+
