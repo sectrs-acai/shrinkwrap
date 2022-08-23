7b19b44da4245d5f972434192813bdc043c44dcd
Ryan Roberts <ryan.roberts@arm.com>
2022-08-23 13:52:17 +0100

    utils: Add process module to manage parallel processes.
    This provides a wrapper around subprocess to enable multiple processes
    to be run in parallel and for their stdout/stderr to be collected and
    neatly multiplexed to the parent process's stdout/stderr.
    
    Signed-off-by: Ryan Roberts <ryan.roberts@arm.com>

---
+++ utils/process.py
@@ -0,0 +1,86 @@
+import fcntl
+import os
+import shlex
+import selectors
+import subprocess
+
+
+class Process:
+	"""
+	A wrapper to a process that should be managed by the ProcessManager.
+	"""
+	def __init__(self, args, interactive, data):
+		self.args = shlex.split(args)
+		self.interactive = interactive
+		self.data = data
+		self._popen = None
+
+
+class ProcessManager:
+	"""
+	A ProcessManager can manage a set of processes running in parallel such
+	that it can capture all their (stdout + stderr) output and pass it to a
+	handler delegate.
+	"""
+	def __init__(self, handler=None):
+		self._handler = handler
+		self._procs = []
+		self._active = 0
+		self._sel = None
+
+	def set_handler(self, handler):
+		self._handler = handler
+
+	def add(self, process):
+		self._procs.append(process)
+		if self._sel:
+			self._activate(process)
+
+	def run(self):
+		try:
+			self._sel = selectors.DefaultSelector()
+			self._active = 0
+
+			for proc in self._procs:
+				self._activate(proc)
+
+			while self._active > 0:
+				for key, mask in self._sel.select():
+					handler = key.data[0]
+					data = key.data[1]
+					handler(key.fileobj, data)
+		finally:
+			for proc in self._procs:
+				if proc._popen:
+					proc._popen.terminate()
+					proc._popen.__exit__(None, None, None)
+					proc._popen = None
+
+			self._sel.close()
+			self._sel = None
+
+	def _register(self, fileobj, data):
+		fl = fcntl.fcntl(fileobj, fcntl.F_GETFL)
+		fcntl.fcntl(fileobj, fcntl.F_SETFL, fl | os.O_NONBLOCK)
+		self._sel.register(fileobj, selectors.EVENT_READ, data)
+
+	def _activate(self, proc):
+		stdin=None if proc.interactive else subprocess.DEVNULL
+
+		proc._popen = subprocess.Popen(proc.args,
+					       stdin=stdin,
+					       stdout=subprocess.PIPE,
+					       stderr=subprocess.STDOUT,
+					       text=True)
+
+		self._register(proc._popen.stdout, (self._proc_handler, proc))
+		self._active += 1
+
+	def _proc_handler(self, fileobj, proc):
+		data = fileobj.read()
+		if data == '':
+			self._sel.unregister(fileobj)
+			self._active -= 1
+		else:
+			if self._handler:
+				self._handler(self, proc, data)
